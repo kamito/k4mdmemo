bfd872309cd672c48e11375e01d1772ad950bf2c
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2014-03-26 19:36:07 +0900

    リストのチェックボックスのスタイルを調整。

--- lib/k4mdmemo/markdown_renderer.rb
+++ lib/k4mdmemo/markdown_renderer.rb
@@ -2,16 +2,18 @@ module K4mdmemo
 
   class MarkdownRenderer < ::Redcarpet::Render::HTML
     def list_item(text, list_type)
+      tag = "<li"
       r = /^\[(\s|x)\]\s/
       m = r.match(text)
       if m
         input_tag = "<input type=\"checkbox\""
-        input_tag << "checked=\"true\"" if m[1] and m[1] == 'x'
+        input_tag << " checked=\"true\"" if m[1] and m[1] == 'x'
         input_tag << ">"
         text = text.gsub(r, '')
         text = "#{input_tag} #{text}"
+        tag << " class=\"task-item\""
       end
-      tag = "<li>#{text}</li>"
+      tag << ">#{text}</li>"
       return tag
     end
   end
